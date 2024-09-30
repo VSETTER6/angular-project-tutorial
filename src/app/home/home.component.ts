ec49f1f33d860c98b703cabc42830b78537aa5cc
VSETTER6 <vilhelm.settergren@live.se>
2024-09-30 15:44:28 +0200

    3.15 into the video

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,30 +1,75 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { ProductsService } from '../services/products.service';
 import { Product, Products } from '../../types';
 import { ProductComponent } from '../components/product/product.component';
 import { CommonModule } from '@angular/common';
-import { PaginatorModule } from 'primeng/paginator';
+import { Paginator, PaginatorModule } from 'primeng/paginator';
+import { EditPopupComponent } from "../components/edit-popup/edit-popup.component";
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
-    ProductComponent, 
+    ProductComponent,
     CommonModule,
-    PaginatorModule
-  ],
+    PaginatorModule,
+    EditPopupComponent,
+    ButtonModule,
+],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-  constructor(
-    private productService: ProductsService
-  ) {}
+  constructor(private productService: ProductsService) {}
+
+  @ViewChild('paginator') paginator: Paginator | undefined;
 
   products: Product[] = [];
 
   totalRecords: number = 0;
   rows = 5;
+
+  displayEditPopup: boolean = false;
+  displayAddPopup: boolean = false;
+
+  toogleEditPopup(product: Product) {
+    this.selectedProduct = product;
+    this.displayEditPopup = true;
+  }
+
+  toogleDeletePopup(product: Product) {
+    if (!product.id) {
+      return;
+    }
+    this.deleteProduct(product.id);
+  }
+
+  toogleAddPopup() {
+    this.displayAddPopup = true;
+  } 
+
+  selectedProduct: Product = {
+    id: 0,
+    name: '',
+    image: '',
+    price: '',
+    rating: 0,
+  }
+
+  onConfirmEdit(product: Product) {
+    if (!this.selectedProduct.id) {
+      return;
+    }
+
+    this.editProduct(product, this.selectedProduct.id);
+    this.displayEditPopup = false;
+  }
+
+  onConfirmAdd(product: Product) {
+    this.addProduct(product)
+    this.displayAddPopup = false;
+  }
 
   onProductOutput(product: Product) {
     console.log(product, 'Output')
@@ -34,11 +79,68 @@ export class HomeComponent {
     this.fetchProducts(event.page, event.rows);
   }
 
+  resetPagintor() {
+    this.paginator?.changePage(0);
+  }
+
   fetchProducts(page: number, perPage: number) {
-    this.productService.getProducts('http://localhost:3000/clothes', { page, perPage}).subscribe((products: Products) => {
-      this.products = products.items;
-      this.totalRecords = products.total;
-    });
+    this.productService.getProducts('http://localhost:3000/clothes', { page, perPage}).subscribe(
+      {
+        next: (data: Products) => {
+          console.log(data);
+          this.products = data.items;
+          this.totalRecords = data.total;
+        },
+        error: (error) => {
+          console.log(error)
+        }
+      }
+    );
+  }
+
+  editProduct(product: Product, id: number) {
+    this.productService.editProduct(`http://localhost:3000/clothes/${id}`, product).subscribe(
+      {
+        next: (data) => {
+          console.log(data);
+          this.fetchProducts(0, this.rows);
+          this.resetPagintor();
+        },
+        error: (error) => {
+          console.log(error)
+        }
+      }
+    );
+  }
+
+  deleteProduct(id: number) {
+    this.productService.deleteProdcut(`http://localhost:3000/clothes/${id}`).subscribe(
+      {
+        next: (data) => {
+          console.log(data);
+          this.fetchProducts(0, this.rows);
+          this.resetPagintor();
+        },
+        error: (error) => {
+          console.log(error)
+        }
+      }
+    );
+  }
+
+  addProduct(product: Product) {
+        this.productService.addProduct(`http://localhost:3000/clothes`, product).subscribe(
+      {
+        next: (data) => {
+          console.log(data);
+          this.fetchProducts(0, this.rows);
+          
+        },
+        error: (error) => {
+          console.log(error)
+        }
+      }
+    );
   }
   
   ngOnInit() {
